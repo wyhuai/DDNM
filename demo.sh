cd789db35c1ed61cc2d4fb61dff46de7796c7aeb
Yinhuai Wang <95485229+wyhuai@users.noreply.github.com>
2022-12-01 15:40:56 +0800

    Delete demo.sh

--- demo.sh
+++
@@ -1,4 +0,0 @@
-# average-pooling-based SR
-python main.py --ni --simplified --config celeba_hq_bs1.yml --path_y celeba_hq --timesteps 100 --eta 0.85 --deg "sr_averagepooling" --deg_scale 4.0 --sigma_y 0 -i demo
-
-
