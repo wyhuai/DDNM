9786235d44fea4de1b24905623eee1024c464d26
Jiwen Yu <46273301+vvictoryuki@users.noreply.github.com>
2022-12-03 13:05:32 +0800

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -15,9 +15,9 @@ python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "
 python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "inpainting" --sigma_y 0. -i celeba_inpainting
 
     # noisy tasks
-#python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "sr_averagepooling" --deg_scale 16 --sigma_y 0.2 -i celeba_sr_ap_16_n_02
+python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "sr_averagepooling" --deg_scale 16 --sigma_y 0.2 -i celeba_sr_ap_16_n_02 --add_noise
 
-#python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "cs_walshhadamard" --deg_scale 0.25 --sigma_y 0.2 -i celeba_cs_wh_025_n_02
+python main.py --ni --config celeba_hq.yml --path_y celeba_hq --eta 0.85 --deg "cs_walshhadamard" --deg_scale 0.25 --sigma_y 0.2 -i celeba_cs_wh_025_n_02 --add_noise
 
 
 ## Experiments on ImageNet ##
