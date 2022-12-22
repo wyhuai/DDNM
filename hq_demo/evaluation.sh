82c244114f2e4dce809f5b808023393953b8cdfd
Yinhuai Wang <95485229+wyhuai@users.noreply.github.com>
2022-12-22 11:29:24 +0800

    Update evaluation.sh

--- hq_demo/evaluation.sh
+++ hq_demo/evaluation.sh
@@ -2,17 +2,16 @@
 ## demos on ImageNet 1K ##
 
     # orange
-python main.py --config confs/inet256.yml --path_y data/datasets/gts/inet256/orange.png --class 950 --deg "sr_averagepooling" --scale 4 -i orange
+python main.py --resize_y --config confs/inet256.yml --path_y data/datasets/gts/inet256/orange.png --class 950 --deg "sr_averagepooling" --scale 4 -i orange
 
     # brown bear
-python main.py --config confs/inet256.yml --path_y data/datasets/gts/inet256/bear.png --class 294 --deg "sr_averagepooling" --scale 4 -i bear
+python main.py --resize_y --config confs/inet256.yml --path_y data/datasets/gts/inet256/bear.png --class 294 --deg "sr_averagepooling" --scale 4 -i bear
 
     # flamingo
-python main.py --config confs/inet256.yml --path_y data/datasets/gts/inet256/flamingo.png --class 130 --deg "sr_averagepooling" --scale 2 -i flamingo
+python main.py --resize_y --config confs/inet256.yml --path_y data/datasets/gts/inet256/flamingo.png --class 130 --deg "sr_averagepooling" --scale 2 -i flamingo
 
     # kimono
-python main.py --config confs/inet256.yml --path_y data/datasets/gts/inet256/kimono.png --class 614 --deg "sr_averagepooling" --scale 2 -i kimono
+python main.py --resize_y --config confs/inet256.yml --path_y data/datasets/gts/inet256/kimono.png --class 614 --deg "sr_averagepooling" --scale 2 -i kimono
 
     # zebra 
-python main.py --config confs/inet256.yml --path_y data/datasets/gts/inet256/zebra.png --class 340 --deg "sr_averagepooling" --scale 4 -i zebra
-
+python main.py --resize_y --config confs/inet256.yml --path_y data/datasets/gts/inet256/zebra.png --class 340 --deg "sr_averagepooling" --scale 4 -i zebra
